3037993b690ba55aa4a8c63af7691c0289d442f0
Sharath Badrinath Pai <sharathpai107@gmail.com>
2023-10-12 17:18:35 +0530

    modified website

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -20,16 +20,16 @@ function Home() {
           </div>
         </div>
       </div>
-      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/>
       <h1>ABOUT MY <span>JOURNEY</span></h1>
       <div className="container">
         <div className="row">
           <div className="col-lg-7 intro">
             <p>I started programming and designing for our startup and it eventually became a passion for me. </p>
             <p>I have a good command over tech stacks such as <span>React, Express, R, etc.</span></p>
-            <p>My areas of interest are building <span>Websites</span> and <span>Data Analysis</span> and I'm also thinking of expanding my skillset in the domain of <span> App Development and Blockchain Development.</span></p>
+            <p>My areas of interest are building <span>Websites</span> and <span>Machine Learning</span> and I'm also thinking of expanding my skillset in the domain of <span> App Development and Blockchain Development.</span></p>
             {/* <p>Currently I'm testing my <span>Backend Dev</span> skills by making websites using <span>Express JS.</span></p> */}
-            <p>Currently I'm working on my <span>Machine Learning</span> skills and making Freelance <span>Websites.</span></p>
+            <p>Currently I'm working on my <span>Machine Learning</span> skills and making <span>Freelance Websites.</span></p>
           </div>
           <div className="col-lg-5 img1">
             <img class="mx-auto d-block" src={require("../assets/man.webp")} />
